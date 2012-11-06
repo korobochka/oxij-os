f6678e5c66c1ea4884e2b4efd9d0f110ef33bb14
Korobochka <ilya-korobitsyn@narod.ru>
2012-11-07 03:05:24 +0400

    Find.sh done

--- bash/find.sh
+++ bash/find.sh
@@ -1,25 +1,61 @@
 #!/bin/bash
 # Find script
 
-list()
+shopt -s dotglob
+shopt -s nocasematch
+shopt -s extglob
+
+check_and_print()
 {
-    #if [-d $1] then
-        for file in $1*
-        do
-            echo $file
-            if [ -d $file ] 
-            then 
-                if [ $1 != $file ] 
-                then 
-                    list $file/
-                fi
-            fi
-        done
-    #fi
-
-
+    if [[ "${1##*/}" != $match ]];
+    then
+        return
+    fi
+    
+    if [[ $type == "" ]] ||
+       [[ ($type == "l" || ! -h $1) &&
+       (($type == "c" && -c $1) ||
+        ($type == "l" && -h $1) ||
+        ($type == "p" && -p $1) ||
+        ($type == "f" && -f $1) ||
+        ($type == "d" && -d $1) ||
+        ($type == "b" && -b $1) ||
+        ($type == "s" && -S $1)) ]];
+    then
+       echo "${1//\/\///}"
+    fi
 }
 
-echo "Hello!"
-list "/home/korobochka/temp/dmd2/"
-echo "World!"
+list()
+{
+    for file in "$1"/*
+    do
+        check_and_print "$file"
+        if [[ -d "$file" ]]
+        then
+            list "$file"
+        fi
+    done
+}
+
+match="*"
+
+
+if [[ $2 == "-iname" ]]
+then
+    match=$3
+fi
+if [[ $4 == "-iname" ]]
+then
+    match=$5
+fi
+if [[ $2 == "-type" ]]
+then
+    type=$3
+fi
+if [[ $4 == "-type" ]]
+then
+    type=$5
+fi
+
+list "$1"
